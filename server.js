68eb67fb2a63ee9a7088bf1fa3105e122a808e95
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 15:13:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Cannot change the region to europe-west1 works only with us region')
+    res.end('New test continuous deployment in Google Cloud Build')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
