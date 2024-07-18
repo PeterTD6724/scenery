99a126475ef77d6df4384ad7d832b09bc348c8fd
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 18:05:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Everything closed computer is in the sleep mode after the commit.')})
+    res.end('Another change when everything is closed - commit 18:05.')})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
