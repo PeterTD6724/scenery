d7196cfb386d3e93c654025e4265ff4b1dcb3aec
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 14:57:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Cannot chante the region to europe-west1')
+    res.end('Cannot change the region to europe-west1 works only with us region')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
