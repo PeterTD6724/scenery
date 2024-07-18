9eb82ee76ff9cf62418f7fbedd7c15bad4030a9d
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 14:53:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello awesome new edit on the repository')
+    res.end('Cannot chante the region to europe-west1')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
