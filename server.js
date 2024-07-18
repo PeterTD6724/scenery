f0381b03cc73863969c7a781658c35462d10834d
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 15:49:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('And the last test for today for automatic restart from GitHub need to be only on one line 15:34')})
+    res.end('Summary of the project.Deploy Docker image in Google Cloud Build with continuous deployment run by Cloud Run.The image is stored in Artifact registry.There is no need of use of Google VM instance to run the project in the browser.')})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
