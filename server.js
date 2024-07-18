fbcbe09cafa54c0aac1c1ee1e001b86be6bdc9cd
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 15:20:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('New test continuous deployment in Google Cloud Build')
+    res.end('New test continuous deployment in Google Cloud Build wth CLOUD RUN AND Docker image in Artifact Registry')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
