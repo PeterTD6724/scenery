bfa9dce0788fa3be10fd9c61253690e5053398f4
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 17:20:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Summary of the project.Deploy Docker image in Google Cloud Build with continuous deployment run by Cloud Run.The image is stored in Artifact registry.There is no need of use of Google VM instance to run the project in the browser.')})
+    res.end('Everything closed computer is in the sleep mode after the commit.')})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
