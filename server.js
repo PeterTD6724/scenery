57828baf1df456a04a8c547e98887fc05db62347
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 13:51:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello awesome edit 1')
+    res.end('Hello awesome new edit on the repository')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
