c92e98f09f75a73eddf09c74c71aeed3b39fc2a7
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 15:37:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('New test for automatic restart need to be only on one line 15:34')})
+    res.end('And the last test for today for automatic restart from GitHub need to be only on one line 15:34')})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
