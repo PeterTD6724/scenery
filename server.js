7d9e010c419ddb6c11f983b327bf62d41a69ee2a
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-18 15:34:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('New test continuous deployment in Google Cloud Build wth CLOUD RUN AND Docker image in Artifact Registry.Additional test on automatic refresh 15:24')})
+    res.end('New test for automatic restart need to be only on one line 15:34')})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
