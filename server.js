ae6e98ae83e79bf05c4eb5766d7a069ca4d87e10
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-19 22:13:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Now we are deleting the repository gcr.io from Artifact registry - commit Fri 19 Jul 22:06.")})
+    res.end("Now we are deleting the repository gcr.io from Artifact registry and the new repository with new image is set - commit Fri 19 Jul 22:13.")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
