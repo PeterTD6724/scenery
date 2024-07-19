4ce557d641b1ca8bf620982b38cc41c0f38a70d9
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-19 20:56:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Start github project - commit Fri 19 Jul 18:10.")})
+    res.end("Finally manage to switch to europe-west1 region image - commit Fri 19 Jul 18:10.")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
