6eca81a39ed0c69c72df1dbcbb4c417df24d4a1e
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-19 23:22:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("added variabe to the trigger the host name for the Artifact repository - commit Fri 19 Jul 22:13.")})
+    res.end("Latest commit before stopping work for today - commit Fri 19 Jul 22:13.")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
