af4fca0d80b9b8d6e30690f46e7feb1e82357ea3
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-19 22:06:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Finally manage to switch to europe-west1 region image - commit Fri 19 Jul 18:10.")})
+    res.end("Now we are deleting the repository gcr.io from Artifact registry - commit Fri 19 Jul 22:06.")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
