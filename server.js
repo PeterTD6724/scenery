2090d230821075fff75b039d53a7c9e01ace879e
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-20 20:36:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Restarting before finish to see what will happen - Commit Sat 20 Jul 20:19.")})
+    res.end("Looks it require page restart to apply the changes - Commit Sat 20 Jul 20:36.")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
