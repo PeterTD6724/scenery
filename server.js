f40847192c52a6bd83aec6b931826d60ecac55b0
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-20 22:44:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("New build with cashing Pull the cached image from Container Registry. - Commit Sat 20 Jul 22:33.")})
+    res.end("Old run  1 min 26 sec latest - Commit Sat 20 Jul 22:33.")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
