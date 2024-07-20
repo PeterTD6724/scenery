151e99936f5df95448338a939ac9e9245af61847
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-20 06:56:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Goog night switching off the fridge.Next job.Deploying Django project on Google build and run it with Cloud run. - commit Fri 19 Jul 23:35.")})
+    res.end("Good morning.The day has just began. - commit Fri 20 Jul 06:56.")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
