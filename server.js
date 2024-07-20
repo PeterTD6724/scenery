ed0e08a71e597c4e0cf8dd6a931421fc28d8a2cb
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-20 20:19:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Start working on Django deployment - Commit Sat 20 Jul 18:28.")})
+    res.end("First deploy the Django projec to github - Commit Sat 20 Jul 20:19.")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
