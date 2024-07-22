5fbc5efaf80402f8a97509ed9747243cd058a54f
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-22 17:24:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Task for the afternoon - cloud run on VS code terminal - Commit Mon 22Jul 11:44")})
+    res.end("Making changes running logs in vs code - Commit Mon 22Jul 17:24")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
