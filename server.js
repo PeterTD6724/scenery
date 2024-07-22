c6c5445cb7ca1240bf9ffbacd9e437b0d9dde297
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-22 21:08:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("New revision set - Commit Mon 22Jul 17:24")})
+    res.end("Change the settings to see how cloud run will work as ssh has problems running linux - Commit Mon 22Jul 21:28")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
