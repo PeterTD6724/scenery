f18fc183f17d5db1e3e848d71915e90e48802861
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-23 11:56:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Good morning Peter have a nice day - Commit Tuesday 23July 07:30")})
+    res.end("Working of VM terminal instance connection - Commit Tuesday 23July 11:55")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
