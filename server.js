7fc9a2377a90ecc0a1063faa112ab8363705d2d2
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-24 17:59:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Goog morning Peter. On what project we are going to work today? Let's move the ubuntu files to new VM / Let's first go again though creating docker image in Artiffact registry with TechTrapture - Commit Wednesday 24 July 09:31")})
+    res.end("Now caching the yaml file for fast cloud build - Commit Wednesday 24 July 09:31")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
