941e9a7a57e288a1037b64c732ae754377efb01f
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-24 18:23:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Now caching the yaml file for fast cloud build - Commit Wednesday 24 July 09:31")})
+    res.end("Now latest modifications with caching for run time save - Commit Wednesday 24 July 09:31")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
