6fe62828166de018ae231001e3cf0f1414f7795c
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-24 20:16:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end(" the chat gpt code is rubbish does not transfer the changes.I will keep mine. doesn't save time but at least doesn't crash - Commit Wednesday 24 July 18:36")})
+    res.end("Another test of the yaml file - Commit Wednesday 24 July 20:15")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
