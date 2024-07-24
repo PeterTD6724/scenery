206a5025b3f3953ecb7ef8121e6986fa89103c98
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-07-24 18:33:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Now latest modifications with caching for run time save - Commit Wednesday 24 July 09:31")})
+    res.end("latest changes wit chat gpt changes and tag latest - Commit Wednesday 24 July 09:31")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
