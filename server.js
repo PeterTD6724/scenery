575de76d11d5e4389e8b10e75d310a0d8b179f26
PeterTD6724 <51209513+PeterTD6724@users.noreply.github.com>
2024-08-02 16:54:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("New test for the day all deleted - Commit Thursday 01 August 2024 16:21")})
+    res.end("Good afternoon Peter - Commit Friday 02 August 2024 16:53")})
 const PORT = process.env.PORT || 8080
 server.listen(PORT,() => console.log('Listening'))
